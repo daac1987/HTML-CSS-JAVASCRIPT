a986f95874d80e6807bb254a6561ee43247a7ffb
davide <davic1987.com>
2023-02-20 22:22:32 -0600

    ejercicio diez

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,9 @@
+// enlaces del ejercicio nueve 
 function ejercicio_nueve_enlaces (){
     window.alert("Los enlaces no tienen asignada la ruta. Son de ejemplo.");
 }
 
+// mostrar imagines originales en todos los ejercicios
 const abrir = document.getElementById("abrir-img-original");
 const divImgOriginal = document.getElementById("img-original");
 const cerrar = document.getElementById("cerrar-img-original");
